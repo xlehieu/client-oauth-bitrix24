040af95280435d181879b16bf7fc87c96f447df8
xlehieu <xlehieu@gmail.com>
2025-06-05 16:14:29 +0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Info from "@/components/Info";
+
+export default function Home() {
+
+  return (
+    <div>
+      <h1>
+        Bitrix24 OAuth Integration
+      </h1>
+      <Info/>
+    </div>
+  );
+}
