5bf58bd307341cc92abfee5def4b743e683a8800
xlehieu <xlehieu@gmail.com>
2025-06-05 17:14:58 +0700

    remote from backend

--- src/app/bitrix/page.tsx
+++ src/app/bitrix/page.tsx
@@ -1,10 +1,12 @@
 // app/bitrix/page.tsx
 'use client';
-
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import Head from 'next/head';
 
 export default function BitrixApp() {
+  const router = useRouter();
+  const { DOMAIN, APP_SID } = router.query;
   useEffect(() => {
     if (typeof window !== 'undefined' && (window as any).BX24) {
       (window as any).BX24.init(() => {
@@ -20,6 +22,9 @@ export default function BitrixApp() {
       </Head>
       <main>
         <h1>Ứng dụng Bitrix của tôi</h1>
+        <p>Domain: {DOMAIN}</p>
+        <p>App SID: {APP_SID}</p>
+        <p>Ứng dụng này đã được cài đặt thành công trên Bitrix24. heeheheh</p>
       </main>
     </>
   );
