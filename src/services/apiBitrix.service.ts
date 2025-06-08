162cf8429152e514dc973375d6fca394d78417fa
xlehieu <xlehieu@gmail.com>
2025-06-08 12:16:00 +0700

    check url

--- src/services/apiBitrix.service.ts
+++ src/services/apiBitrix.service.ts
@@ -4,8 +4,9 @@ export const getDataApiBitrix = async (action: string, payload: any) => {
     try {
         // /api/call
         const response = await axiosInterceptor.post(`/api/call`, JSON.stringify({ action, payload }));
+        console.error('URL:', axiosInterceptor.defaults.baseURL);
         return response.data;
     } catch (error) {
-        console.log(error);
+        console.log('ERROR CALL API BITRIX24', error);
     }
 };
