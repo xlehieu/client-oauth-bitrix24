041375633c3f65a53820f7c19f2d1b97f4ed535a
xlehieu <xlehieu@gmail.com>
2025-06-08 21:16:38 +0700

    fix axios

--- src/config/axios.interceptor.ts
+++ src/config/axios.interceptor.ts
@@ -7,24 +7,24 @@ const axiosInterceptor = axios.create({
     },
     //beforeRedirect
 });
-// axiosInterceptor.interceptors.request.use((config) => {
-//     const token = localStorage.getItem('access_token');
-//     console.log('TOKEN LOCAL STORAGE: ', token);
-//     if (token) {
-//         config.headers.Authorization = 'Bearer ' + token;
-//     }
-//     return config;
-// });
-// axiosInterceptor.interceptors.response.use(
-//     (response) => response, // Trả về reponse nếu không có lỗi,)
-//     (error) => {
-//         if (error.response?.status === 401) {
-//             window.location.href = '/auth/login';
-//             return;
-//         } else if ([403, 404].includes(error.response?.status)) {
-//             window.location.href = '/page-not-found';
-//         }
-//         return Promise.reject(error); // Trả về l��i nếu có l��i
-//     },
-// );
+axiosInterceptor.interceptors.request.use((config) => {
+    const token = localStorage.getItem('access_token');
+    console.log('TOKEN LOCAL STORAGE: ', token);
+    if (token) {
+        config.headers.Authorization = 'Bearer ' + token;
+    }
+    return config;
+});
+axiosInterceptor.interceptors.response.use(
+    (response) => response, // Trả về reponse nếu không có lỗi,)
+    (error) => {
+        if (error.response?.status === 401) {
+            window.location.href = '/auth/login';
+            return;
+        } else if ([403, 404].includes(error.response?.status)) {
+            window.location.href = '/page-not-found';
+        }
+        return Promise.reject(error); // Trả về l��i nếu có l��i
+    },
+);
 export default axiosInterceptor;
