fb81e13e15cbdeec7c3c3acbf316188bac0e1d88
xlehieu <xlehieu@gmail.com>
2025-06-08 16:48:42 +0700

    ok ok

--- src/config/axios.nointerceptor.ts
+++ src/config/axios.nointerceptor.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 const axiosNoInterceptor = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
-    withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
     },
